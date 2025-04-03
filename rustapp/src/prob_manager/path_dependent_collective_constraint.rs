b63afda494cbcc9a8387f7430e3c003b5ac51764
OYASHI777 <oyashi7@gmail.com>
2025-04-03 18:27:39 +0800

    Added some template

--- rustapp/src/prob_manager/path_dependent_collective_constraint.rs
+++ rustapp/src/prob_manager/path_dependent_collective_constraint.rs
@@ -1,16 +1,79 @@
 use crate::history_public::Card;
 use super::compressed_group_constraint::CompressedGroupConstraint;
 
-pub enum SignificantAction {
-    Discard {player: u8, card: Card}, // Player | Card
-    RevealRedraw {player: u8, reveal: Card, redraw: Option<Card>}, // Player | Reveal Card | Redraw Option<Card>
-    ExchangeDrawChoice {player: u8, draw: Vec<Card>, relinquish: Vec<Card>}, // Player | Draw Vec<Card> | Return Vec<Card>
+pub enum ActionInfo {
+    Discard {discard: Card}, // Player | Card
+    RevealRedraw {reveal: Card, redraw: Option<Card>}, // Player | Reveal Card | Redraw Option<Card>
+    ExchangeDrawChoice {draw: Vec<Card>, relinquish: Vec<Card>}, // Player | Draw Vec<Card> | Return Vec<Card>
 }
 
-pub struct ActionMetaData {
+impl ActionInfo {
+    /// All private information is known
+    pub fn fully_known(&self) -> bool {
+        match self {
+            Self::Discard{ .. } => {
+                true
+            },
+            Self::RevealRedraw{ redraw, .. } => {
+                redraw.is_some()
+            },
+            Self::ExchangeDrawChoice {draw, relinquish} => {
+                draw.len() == 2 && relinquish.len() == 2
+            }
+        }
+    }
+    /// At least some private information known, or no private information to know
+    pub fn partially_known(&self) -> bool {
+        match self {
+            Self::Discard{ .. } => {
+                true
+            },
+            Self::RevealRedraw{ redraw, .. } => {
+                redraw.is_some()
+            },
+            Self::ExchangeDrawChoice {draw, relinquish} => {
+                draw.len() > 0 || relinquish.len() > 0
+            }
+        }
+    }
+    /// No private information is known
+    pub fn fully_unknown(&self) -> bool {
+        match self {
+            Self::Discard{ .. } => {
+                false
+            },
+            Self::RevealRedraw{ redraw, .. } => {
+                redraw.is_none()
+            },
+            Self::ExchangeDrawChoice {draw, relinquish} => {
+                draw.len() == 0 && relinquish.len() == 0
+            }
+        }
+    }
+}
+pub struct SignificantAction {
     move_no: usize,
-    action: SignificantAction,
+    player: u8,
+    action_info: ActionInfo,
     meta_data: PathDependentMetaData,
+}
+
+impl SignificantAction {
+    pub fn move_no(&self) -> usize {
+        self.move_no
+    }
+    pub fn player(&self) -> u8 {
+        self.player
+    }
+    pub fn action_info(&self) -> &ActionInfo {
+        &self.action_info
+    }
+    pub fn action_info_mut(&mut self) -> &mut ActionInfo {
+        &mut self.action_info
+    }
+    pub fn meta_data(&self) -> &PathDependentMetaData {
+        &self.meta_data
+    }
 }
 
 // TODO: implement Into<PathDependentMetaData> for PathDependentCollectiveConstraint
@@ -19,6 +82,18 @@ pub struct PathDependentMetaData {
     inferred_constraints: Vec<Vec<Card>>,
     impossible_constraints: [[bool; 5]; 7], // For each player store an array of bool where each index is a Card, this represents whether a player cannot have a card true => cannot
 }
+
+impl PathDependentMetaData {
+    pub fn public_constraints(&self) -> &Vec<Vec<Card>> {
+        &self.public_constraints
+    }
+    pub fn inferred_constraints(&self) -> &Vec<Vec<Card>> {
+        &self.inferred_constraints
+    }   
+    pub fn impossible_constraints(&self) -> &[[bool; 5]; 7] {
+        &self.impossible_constraints
+    }   
+}   
 
 #[derive(Clone)]
 /// A struct that helps in card counting. Stores all information known about cards by a particular player.
@@ -40,9 +115,9 @@ pub struct PathDependentCollectiveConstraint {
     //      => only concern is a possible case wehre single_card_flag should be false because it was redrawn, but we not sure where it is redrawn
     // revealed_status: Vec<Vec<(Option<Card>, usize)>>, 
     move_no: usize, // turn number
-    history: Vec<ActionMetaData>, // Stores 
+    history: Vec<SignificantAction>, // Stores 
     // The shared LRU cache is maintained here and passed to each constraint.
-    // cache: Rc<RefCell<LruCache<ConstraintKey, ActionMetaData>>>,
+    // cache: Rc<RefCell<LruCache<ConstraintKey, SignificantAction>>>,
     // Revealed_status stores the cards and the players that have reveal_redrawn, and have yet to use ambassador (mix)
     // When reveal_redraw is done, the card is added for the corresponding player
     // If player has mixed, it gets emptied.
@@ -130,8 +205,58 @@ impl PathDependentCollectiveConstraint {
     /// Create a method to understand for the some discard/inferred card, whether any previous move's 
     /// hidden information is known
     /// or actually maybe don't need this, but you can repeat on the latest card over and over?
-    pub fn lookback_1(&self, index: usize) {
+    /// might need to look forward too?
+    pub fn lookback_1(&mut self, index: usize) -> bool {
         // index is the index for history
+        let considered_move: &SignificantAction = &self.history[index];
+        match considered_move.action_info() {
+            ActionInfo::RevealRedraw(reveal_considered, redraw_considered) => {
+                match redraw_considered {
+                    Some(redraw) => {
+
+                    },
+                    None => {
+                        
+                    }
+                }
+            },
+            ActionInfo::Discard(discard_considered) => {
+                let player_index: u8 = considered_move.player();
+                for i in (0..index).rev() {
+                    let action_data = &self.history[i];
+                    let action_player = action_data.player();
+                    if action_player == player_index {
+                        // Case 0
+                        // RR or AMB with 1 life
+                        match action_data.action_info_mut() {
+                            ActionInfo::RevealRedraw(reveal_current, redraw_current) => {
+                                // 1 card unknown
+                                if redraw_current.is_none() 
+                                && action_data.public_constraints()[action_player as usize].len() + action_data.inferred_constraints()[action_player as usize].len() == 1 
+                                && !action_data.public_constraints()[action_player as usize].contains(&discard_considered)
+                                && !action_data.inferred_constraints()[action_player as usize].contains(&discard_considered)
+                                {
+                                    *redraw_current = Some(discard_considered);
+                                    // TODO: recurse
+                                    return true;
+                                }
+                            },
+                            ActionInfo::ExchangeDrawChoice(draw, relinquish) => {
+        
+                            },
+                            ActionInfo::Discard(discard) => {}
+                        }
+                    }
+                    unimplemented!();
+                }
+            },
+            _ => {
+                unimplemented!();
+            }
+        }
+    }
+    /// Your generic lookback for an inferred constraint
+    pub fn lookback_2(&self, index: usize) {
 
     }
     /// Recalculate current Constraint from scratch using history
