bd34e38dfdd7a0fe51a497f03836ab04fd956c27
OYASHI777 <oyashi7@gmail.com>
2025-04-03 17:10:26 +0800

    More template code

---
+++ rustapp/src/prob_manager/path_dependent_collective_constraint.rs
@@ -0,0 +1,106 @@
+use crate::history_public::Card;
+use super::compressed_group_constraint::CompressedGroupConstraint;
+
+pub enum SignificantAction {
+    Discard,
+    RevealRedraw,
+    ExchangeDrawChoice,
+}
+
+pub struct ActionMetaData {
+    move_no: usize,
+    action: SignificantAction,
+    meta_data: PathDependentMetaData,
+}
+
+// TODO: implement Into<PathDependentMetaData> for PathDependentCollectiveConstraint
+pub struct PathDependentMetaData {
+    public_constraints: Vec<Vec<Card>>,
+    inferred_constraints: Vec<Vec<Card>>,
+    impossible_constraints: [[bool; 5]; 7], // For each player store an array of bool where each index is a Card, this represents whether a player cannot have a card true => cannot
+}
+
+#[derive(Clone)]
+/// A struct that helps in card counting. Stores all information known about cards by a particular player.
+pub struct PathDependentCollectiveConstraint {
+    // public_constraints:[[Option<Card>; 2]; 6], // Stores all the dead cards of dead players, None are all behind
+    public_constraints: Vec<Vec<Card>>, // Stores all the dead cards of dead players, None are all behind
+    // inferred_constraints:[[Option<Card>; 2]; 6], // Stores all the dead cards of dead players 
+    inferred_constraints: Vec<Vec<Card>>, // Stores all the inferred cards of alive players 
+    // Struct of arrays makes it more efficient to process rather than an array of structs
+    group_constraints_amb: Vec<CompressedGroupConstraint>,
+    group_constraints_ass: Vec<CompressedGroupConstraint>,
+    group_constraints_cap: Vec<CompressedGroupConstraint>,
+    group_constraints_duk: Vec<CompressedGroupConstraint>,
+    group_constraints_con: Vec<CompressedGroupConstraint>,
+    impossible_constraints: [[bool; 5]; 7], // For each player store an array of bool where each index is a Card, this represents whether a player cannot have a card true => cannot
+    revealed_status: Vec<Vec<(Option<Card>, usize)>>, 
+    move_no: usize, // turn number
+    history: Vec<ActionMetaData>, // Stores 
+    // The shared LRU cache is maintained here and passed to each constraint.
+    // cache: Rc<RefCell<LruCache<ConstraintKey, ActionMetaData>>>,
+    // Revealed_status stores the cards and the players that have reveal_redrawn, and have yet to use ambassador (mix)
+    // When reveal_redraw is done, the card is added for the corresponding player
+    // If player has mixed, it gets emptied.
+    // a card can be removed from revealed_status on Discard too, though it may not update all groups
+}
+
+impl PathDependentCollectiveConstraint {
+    pub fn game_start() -> Self {
+        let public_constraints: Vec<Vec<Card>> = vec![Vec::with_capacity(2),Vec::with_capacity(2),Vec::with_capacity(2),Vec::with_capacity(2),Vec::with_capacity(2),Vec::with_capacity(2),Vec::new()]; 
+        let inferred_constraints: Vec<Vec<Card>> = vec![Vec::with_capacity(2),Vec::with_capacity(2),Vec::with_capacity(2),Vec::with_capacity(2),Vec::with_capacity(2),Vec::with_capacity(2),Vec::with_capacity(3)]; 
+        let mut group_constraints_amb: Vec<CompressedGroupConstraint> = Vec::with_capacity(5);
+        let mut group_constraints_ass: Vec<CompressedGroupConstraint> = Vec::with_capacity(5);
+        let mut group_constraints_cap: Vec<CompressedGroupConstraint> = Vec::with_capacity(5);
+        let mut group_constraints_duk: Vec<CompressedGroupConstraint> = Vec::with_capacity(5);
+        let mut group_constraints_con: Vec<CompressedGroupConstraint> = Vec::with_capacity(5);
+        let mut card_num_constraint: CompressedGroupConstraint = CompressedGroupConstraint::zero();
+        for i in 0..7 {
+            card_num_constraint.set_player_flag(i, true);
+        }
+        card_num_constraint.set_alive_count(3);
+        card_num_constraint.set_total_count(3);
+        card_num_constraint.set_card(Card::Ambassador);
+        group_constraints_amb.push(card_num_constraint);
+        card_num_constraint.set_card(Card::Assassin);
+        group_constraints_ass.push(card_num_constraint);
+        card_num_constraint.set_card(Card::Captain);
+        group_constraints_cap.push(card_num_constraint);
+        card_num_constraint.set_card(Card::Duke);
+        group_constraints_duk.push(card_num_constraint);
+        card_num_constraint.set_card(Card::Contessa);
+        group_constraints_con.push(card_num_constraint);
+        let impossible_constraints: [[bool; 5]; 7] = [[false; 5]; 7];
+        let dead_card_count: [u8; 5] = [0; 5];
+        let inferred_card_count: [u8; 5] = [0; 5];
+        let revealed_status = vec![Vec::with_capacity(5); 7];
+        // TODO: Add inferred_card_count
+        Self {
+            public_constraints,
+            inferred_constraints,
+            group_constraints_amb,
+            group_constraints_ass,
+            group_constraints_cap,
+            group_constraints_duk,
+            group_constraints_con,
+            impossible_constraints,
+            dead_card_count,
+            inferred_card_count,
+            revealed_status,
+            move_no: 0,
+        }
+    }
+    /// Create a method to understand for the latest discard/inferred card, whether some previous move's 
+    /// hidden information is known
+    pub fn lookback_0(&self) {
+
+    }
+    /// Create a method to understand for the some discard/inferred card, whether any previous move's 
+    /// hidden information is known
+    /// or actually maybe don't need this, but you can repeat on the latest card over and over?
+    pub fn lookback_1(&self, index: usize) {
+        // index is the index for history
+
+    }
+    // Add other normal methods for inference
+}
--- rustapp/src/prob_manager/path_dependent_prob.rs
+++ rustapp/src/prob_manager/path_dependent_prob.rs
@@ -5,14 +5,14 @@
 // Tried instead to save into hashmap and store in bson
 
 use crate::history_public::{Card, AOName, ActionObservation};
-use super::collective_constraint::CompressedCollectiveConstraint;
+use super::path_dependent_collective_constraint::PathDependentCollectiveConstraint;
 
 pub struct PathDependentCardCountManager {
     // a vec of constraints to push and pop
     // dead cards to push or pop
     // Will not locally store game history, jsut the constraint history
-    game_start: CompressedCollectiveConstraint, // To use a different PathDependentCompressedCollectiveConstraint
-    constraint_history: Vec<CompressedCollectiveConstraint>, // I think None is stored if there are no changes
+    game_start: PathDependentCollectiveConstraint, // To use a different PathDependentPathDependentCollectiveConstraint
+    constraint_history: Vec<PathDependentCollectiveConstraint>, // I think None is stored if there are no changes
     constraint_history_move_no: Vec<usize>, // TODO: determine if more optimal to put in constraint_history
     move_number: usize,
 }
@@ -20,16 +20,16 @@ impl PathDependentCardCountManager {
     /// Constructor
     pub fn new() -> Self {
         PathDependentCardCountManager{
-            game_start: CompressedCollectiveConstraint::game_start(),
+            game_start: PathDependentCollectiveConstraint::game_start(),
             constraint_history: Vec::with_capacity(120),
             constraint_history_move_no: Vec::with_capacity(120),
-            move_number: 0, // First move will be move 0, post-increment this
+            move_number: 1, // First move will be move 1, post-increment this (saving 0 for initial game state)
         }
     }
     /// Returns everything to original state
     pub fn reset(&mut self) {
         self.constraint_history = Vec::with_capacity(120);
-        self.move_no = 0;
+        self.move_no = 1;
     }
     /// Logs the constraint's log
     pub fn printlog(&self) {
@@ -37,25 +37,19 @@ impl PathDependentCardCountManager {
         if let Some(constraint) = self.constraint_history.last() {
             constraint.printlog();
         } else {
-            // How on earth did I print an empty state for others, this is pretty shitty design
             self.game_start.printlog();
         }
     }
     /// Gets the Latest Constraint
-    pub fn latest_constraint(&self) -> CompressedCollectiveConstraint {
-        // TODO: maybe not use this shitty API where I am always forced to clone?
-        if Some(val) = self.constraint_history.last() {
-            val.clone()
-        } else {
-            self.game_start.clone()
-        }
+    pub fn latest_constraint(&self) -> &PathDependentCollectiveConstraint {
+        self.constraint_history.last().unwrap_or(&self.game_start)
     }
     /// Entrypoint for any action done, updates history accordingly
     pub fn push_ao(&mut self, ao: &ActionObservation, bool_know_priv_info: bool){
 
         // Handle different move types
         unimplemented!();
-
+        // shove move_no into CollectiveConstraint
         // post_increment: move_no is now the number of the next move
         self.move_no += 1;
     }
