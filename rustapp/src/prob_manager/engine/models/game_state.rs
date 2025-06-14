abee5c7315671e3e6d8744e2b6f725d75895e827
OYASHI777 <oyashi7@gmail.com>
2025-06-14 15:38:45 +0800

    Added a check for if game will be won

--- rustapp/src/prob_manager/engine/models/game_state.rs
+++ rustapp/src/prob_manager/engine/models/game_state.rs
@@ -35,6 +35,23 @@ impl GameData {
         }
         self.player_turn = current_turn;
     }
+    /// Checks if game will be won after a Discard
+    pub fn game_will_be_won(&self, action: &ActionObservation) -> bool {
+        match action {
+            ActionObservation::Discard { player_id, card, no_cards } => {
+                self.influence.iter().enumerate().filter(
+                    |(p, l)| {
+                        if *p != *player_id {
+                            **l > 0
+                        } else {
+                            **l > *no_cards as u8
+                        }
+                    }
+                ).count() == 1
+            },
+            _ => false,
+        }
+    }
 }
 
 pub struct GameState {
