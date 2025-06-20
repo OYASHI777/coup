e2ae433a0a1f3b3c8eb195a01adb3a7c76079739
OYASHI777 <oyashi7@gmail.com>
2025-06-20 09:53:17 +0800

    refactor - cleanup

--- rustapp/src/prob_manager/tracker/uninformed_tracker.rs
+++ rustapp/src/prob_manager/tracker/uninformed_tracker.rs
@@ -1,125 +1,127 @@
 use crate::traits::prob_manager::coup_analysis::CoupGeneration;
-
+use crate::prob_manager::engine::models_prelude::*;
+use crate::prob_manager::engine::models::game_state::GameData;
+use crate::history_public::ActionObservation;
 pub struct UninformedTracker;
 
 impl CoupGeneration for UninformedTracker {
-    fn on_turn_start(&self, state: &crate::prob_manager::engine::models::turn_start::TurnStart, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_turn_start(&self, state: &TurnStart, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_end(&self, state: &crate::prob_manager::engine::models::end::End, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_end(&self, state: &End, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_coup_hit(&self, state: &crate::prob_manager::engine::models::coup::CoupHit, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_coup_hit(&self, state: &CoupHit, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_foreign_aid_invites_block(&self, state: &crate::prob_manager::engine::models::foreign_aid::ForeignAidInvitesBlock, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_foreign_aid_invites_block(&self, state: &ForeignAidInvitesBlock, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_foreign_aid_block_invites_challenge(&self, state: &crate::prob_manager::engine::models::foreign_aid::ForeignAidBlockInvitesChallenge, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_foreign_aid_block_invites_challenge(&self, state: &ForeignAidBlockInvitesChallenge, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_foreign_aid_block_challenged(&self, state: &crate::prob_manager::engine::models::foreign_aid::ForeignAidBlockChallenged, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_foreign_aid_block_challenged(&self, state: &ForeignAidBlockChallenged, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_foreign_aid_block_challenger_failed(&self, state: &crate::prob_manager::engine::models::foreign_aid::ForeignAidBlockChallengerFailed, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_foreign_aid_block_challenger_failed(&self, state: &ForeignAidBlockChallengerFailed, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_tax_invites_challenge(&self, state: &crate::prob_manager::engine::models::tax::TaxInvitesChallenge, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_tax_invites_challenge(&self, state: &TaxInvitesChallenge, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_tax_challenged(&self, state: &crate::prob_manager::engine::models::tax::TaxChallenged, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_tax_challenged(&self, state: &TaxChallenged, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_tax_challenger_failed(&self, state: &crate::prob_manager::engine::models::tax::TaxChallengerFailed, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_tax_challenger_failed(&self, state: &TaxChallengerFailed, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_steal_invites_challenge(&self, state: &crate::prob_manager::engine::models::steal::StealInvitesChallenge, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_steal_invites_challenge(&self, state: &StealInvitesChallenge, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_steal_challenged(&self, state: &crate::prob_manager::engine::models::steal::StealChallenged, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_steal_challenged(&self, state: &StealChallenged, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_steal_challenger_failed(&self, state: &crate::prob_manager::engine::models::steal::StealChallengerFailed, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_steal_challenger_failed(&self, state: &StealChallengerFailed, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_steal_invites_block(&self, state: &crate::prob_manager::engine::models::steal::StealInvitesBlock, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_steal_invites_block(&self, state: &StealInvitesBlock, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_steal_block_invites_challenge(&self, state: &crate::prob_manager::engine::models::steal::StealBlockInvitesChallenge, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_steal_block_invites_challenge(&self, state: &StealBlockInvitesChallenge, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_steal_block_challenged(&self, state: &crate::prob_manager::engine::models::steal::StealBlockChallenged, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_steal_block_challenged(&self, state: &StealBlockChallenged, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_steal_block_challenger_failed(&self, state: &crate::prob_manager::engine::models::steal::StealBlockChallengerFailed, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_steal_block_challenger_failed(&self, state: &StealBlockChallengerFailed, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_exchange_invites_challenge(&self, state: &crate::prob_manager::engine::models::exchange::ExchangeInvitesChallenge, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_exchange_invites_challenge(&self, state: &ExchangeInvitesChallenge, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_exchange_drawing(&self, state: &crate::prob_manager::engine::models::exchange::ExchangeDrawing, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_exchange_drawing(&self, state: &ExchangeDrawing, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_exchange_drawn(&self, state: &crate::prob_manager::engine::models::exchange::ExchangeDrawn, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_exchange_drawn(&self, state: &ExchangeDrawn, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_exchange_challenged(&self, state: &crate::prob_manager::engine::models::exchange::ExchangeChallenged, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_exchange_challenged(&self, state: &ExchangeChallenged, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_exchange_challenger_failed(&self, state: &crate::prob_manager::engine::models::exchange::ExchangeChallengerFailed, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_exchange_challenger_failed(&self, state: &ExchangeChallengerFailed, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_assassinate_invites_challenge(&self, state: &crate::prob_manager::engine::models::assassinate::AssassinateInvitesChallenge, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_assassinate_invites_challenge(&self, state: &AssassinateInvitesChallenge, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_assassinate_invites_block(&self, state: &crate::prob_manager::engine::models::assassinate::AssassinateInvitesBlock, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_assassinate_invites_block(&self, state: &AssassinateInvitesBlock, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_assassinate_block_invites_challenge(&self, state: &crate::prob_manager::engine::models::assassinate::AssassinateBlockInvitesChallenge, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_assassinate_block_invites_challenge(&self, state: &AssassinateBlockInvitesChallenge, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_assassinate_block_challenged(&self, state: &crate::prob_manager::engine::models::assassinate::AssassinateBlockChallenged, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_assassinate_block_challenged(&self, state: &AssassinateBlockChallenged, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_assassinate_block_challenger_failed(&self, state: &crate::prob_manager::engine::models::assassinate::AssassinateBlockChallengerFailed, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_assassinate_block_challenger_failed(&self, state: &AssassinateBlockChallengerFailed, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_assassinate_succeeded(&self, state: &crate::prob_manager::engine::models::assassinate::AssassinateSucceeded, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_assassinate_succeeded(&self, state: &AssassinateSucceeded, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_assassinate_challenged(&self, state: &crate::prob_manager::engine::models::assassinate::AssassinateChallenged, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_assassinate_challenged(&self, state: &AssassinateChallenged, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 
-    fn on_assassinate_challenger_failed(&self, state: &crate::prob_manager::engine::models::assassinate::AssassinateChallengerFailed, data: &crate::prob_manager::engine::models::game_state::GameData) -> Vec<crate::history_public::ActionObservation> {
+    fn on_assassinate_challenger_failed(&self, state: &AssassinateChallengerFailed, data: &GameData) -> Vec<ActionObservation> {
         todo!()
     }
 }
