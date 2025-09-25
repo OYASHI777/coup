3a48e3042fc596089c635a635ceb6645e952d708
OYASHI777 <oyashi7@gmail.com>
2025-09-25 11:33:21 +0800

    constants: removed unused constants

--- rustapp/src/prob_manager/constants.rs
+++ rustapp/src/prob_manager/constants.rs
@@ -1,6 +1,3 @@
-use lazy_static::lazy_static;
-use std::collections::HashMap; //This allows us to declare "constant" dynamic variables allowing them to run just once in runtime
-
 // A list of constants
 pub const TOKENS: &str = "AAABBBCCCDDDEEE";
 
@@ -9,32 +6,4 @@ pub const TOKENS: &str = "AAABBBCCCDDDEEE";
 pub const MAX_HAND_STATES: usize = 107940;
 pub const MAX_PERM_STATES: usize = 1469700; // See test
 pub const MAX_GAME_LENGTH: usize = 120;
-pub static ARR_POSSIBLE_HANDS: [&str; 15] = [
-    "AA", "AB", "AC", "AD", "AE", "BB", "BC", "BD", "BE", "CC", "CD", "CE", "DD", "DE", "EE",
-];
-lazy_static! {
-    pub static ref BAG_SIZES: Vec<usize> = vec![2, 2, 2, 2, 2, 2, 3];
-    //consider making the indices based on BAG_SIZES
-
-    // pub static ref VEC_POSSIBLE_HANDS: Vec<String> = super::permutation_generator::gen_bag_combinations(TOKENS, &PLAYER_HAND_SIZE);
-
-    // These variables represent a mapping for which index represents which hand one might have e.g. "AB" or "AA"
-    // To be used to index into the conditional policy
-    // pub static ref MAP_CONDHAND_INDEX: HashMap<String, usize> = VEC_POSSIBLE_HANDS.iter().enumerate().map(|(index, key)| (key.clone(), index)).collect();
-    // pub static ref MAP_INDEX_CONDHAND: HashMap<usize, String> = VEC_POSSIBLE_HANDS.iter().enumerate().map(|(index, key)| (index, key.clone())).collect();
-
-    pub static ref MAP_CONDHAND_INDEX: HashMap<&'static str, usize> = {
-        let mut m = HashMap::new();
-        for (index, &hand) in ARR_POSSIBLE_HANDS.iter().enumerate(){
-            m.insert(hand, index);
-        }
-        m
-    };
-    pub static ref MAP_INDEX_CONDHAND: HashMap<usize, &'static str> = {
-        let mut m = HashMap::new();
-        for (index, &hand) in ARR_POSSIBLE_HANDS.iter().enumerate(){
-            m.insert(index, hand);
-        }
-        m
-    };
-}
+pub const BAG_SIZES: [usize; 7] = [2, 2, 2, 2, 2, 2, 3];
